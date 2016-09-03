e6e3acbde590dc35da76c66a4bc1bd3beb95ebb4
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-09-03 09:09:43 -0400

    Completed

--- Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Tuples.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,10 +14,7 @@
 /*: question1
  ### 1. Create a tuple to represent a person! The tuple should have a person's name in the first field and their age in the second field. Define the tuple as a variable named `person`.
  */
-// write your code here
-
-
-
+var person = (name: "Arvin", age: 27)
 
 
 
@@ -26,7 +23,7 @@
 /*: question2
  ### 2. Print out "<Name> is <Age> years old" using the tuple you created in Question 1.
  */
-// write your code here
+print("\(person.name) is \(person.age) years old.")
 
 
 
@@ -34,11 +31,12 @@
 
 
 
+var birthday = true
+if birthday {
+    person.age += 1
+    print("\(person.name) is \(person.age) years old.")
 
-/*: question3
- ### 3. It is the person's birthday! Increment the person's age by one year that you created in Question 1.
- */
-// write your code here
+}
 
 
 
@@ -50,9 +48,7 @@
 /*: question4
  ### 4. Print out "Happy <Age> birthday, <Name>!" using the `person` variable.
  */
-// write your code here
-
-
+print("Happy \(person.age) birthday, \(person.name)!")
 
 
 
@@ -62,8 +58,7 @@
 /*: question5
  ### 5. Create another person variable called `someone` using _named_ fields for the name and age.
  */
-// write your code here
-
+var someone = (name: "Shiela", age: 27)
 
 
 
@@ -74,8 +69,7 @@
 /*: question6
  ### 6. Print out "<Name> is <Age> years old" for the `someone` variable you created in Question 5.
  */
-// write your code here
-
+print("\(someone.name) is \(someone.age) years old.")
 
 
 
@@ -85,10 +79,12 @@
 /*:
  ### 7. It is `someone`'s birthday! Increment `someone`'s age by one year.
  */
-// write your code here
+var someoneBirthday = true
+if someoneBirthday {
+    someone.age += 1
+    print("\(someone.name) is \(someone.age) years old.")
 
-
-
+}
 
 
 
@@ -96,8 +92,7 @@
 /*:
  ### 8. Print out "Happy <Age> birthday, <Name>!" using the `someone` variable.
  */
-// write your code here
-
+print("Happy \(someone.age) birthday, \(someone.name)")
 
 
 
@@ -108,11 +103,7 @@
 /*:
  ### 9. Create a _constant_ tuple called `human`, with named fields for `name` and `age`.
  */
-// write your code here
-
-
-
-
+let human = (name: "Billy", age: 25)
 
 
 
@@ -120,7 +111,7 @@
 /*:
  ### 10. Can you change `human`'s age? Why or why not?
  */
-
+// human.age = 30
 
 
 
